3e0d91987aa7bb9d3b75f03b9bb6afe9c717ba52
Alexander Bosikov <legionerab3000@yandex.ru>
2022-07-03 13:53:09 +0300

    Remove users object from router

--- src/user-router.js
+++ src/user-router.js
@@ -3,17 +3,6 @@ const { getUsers, createUser } = require('./user-controller');
 
 const router = new Router();
 
-const users = [
-  {
-    id: 1,
-    name: 'Petya',
-  },
-  {
-    id: 2,
-    name: 'Vasia',
-  },
-];
-
 router.get('/users', (req, res) => {
   getUsers(req, res);
   res.end();
